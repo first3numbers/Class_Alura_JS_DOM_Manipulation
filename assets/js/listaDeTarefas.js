d17b9e9678de0c3cf480c9fc6b09905350cf7a67
first3numbers <marcelohac2016@gmail.com>
2022-05-17 16:58:13 -0300

    class finished

--- assets/js/listaDeTarefas.js
+++
@@ -1,7 +0,0 @@
-const novaTarefa = document.querySelector("[data-form-button]")
-
-const input = document.querySelector("[data-form-input]")
-
-novaTarefa.addEventListener("click", () => {
-    console.log(input.value)
-})
